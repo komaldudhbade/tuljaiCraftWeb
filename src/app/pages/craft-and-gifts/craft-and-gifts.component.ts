5eccaa93427c16e358a53dbd3f249368166d9f03
Komal Dudhbade <komaldudhbade.dev@gmail.com>
2022-02-03 14:50:37 +0530

    update home page

--- src/app/pages/craft-and-gifts/craft-and-gifts.component.ts
+++ src/app/pages/craft-and-gifts/craft-and-gifts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { STATIC_DATA_PATH, STATIC_IMAGES_PATH } from 'src/app/app-constants';
 
 @Component({
   selector: 'app-craft-and-gifts',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./craft-and-gifts.component.scss']
 })
 export class CraftAndGiftsComponent implements OnInit {
-
+  public dataPath: string = STATIC_DATA_PATH;
+  public imagePath: string = STATIC_IMAGES_PATH+"item1.jpg";
+  public list: number[] = [1,2,3,4,6,7,8,9];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/handmade-stuff/handmade-stuff.component.ts
+++ src/app/pages/handmade-stuff/handmade-stuff.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { STATIC_DATA_PATH, STATIC_IMAGES_PATH } from 'src/app/app-constants';
 
 @Component({
   selector: 'app-handmade-stuff',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./handmade-stuff.component.scss']
 })
 export class HandmadeStuffComponent implements OnInit {
-
+  public dataPath: string = STATIC_DATA_PATH;
+  public imagePath: string = STATIC_IMAGES_PATH+"item1.jpg";
+  public list: number[] = [1,2,3,4,6,7,8,9];
   constructor() { }
 
   ngOnInit(): void {
